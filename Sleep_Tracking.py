ddb10a66fd1e32bac478a2b1e087a662a88a97e1
joshuawiley <joshuaw1@kpit.com>
2018-07-25 16:51:46 -0400

    Added Summary

--- Sleep_Tracking.py
+++ Sleep_Tracking.py
@@ -19,10 +19,9 @@ plt.axhline(y=0)
 # Draw a default vline at x=1 that spans the yrange
 plt.axvline(x=0)
 
-# plt.plot(ms, X_Axis, "g")
 plt.plot(ms, X_Axis, 'r', ms, Y_Axis, 'g', ms, Z_Axis, 'b')
 plt.minorticks_on()
-plt.title('Sleep Tracking ----- Speed vs Time ')
+plt.title('Sleep Tracking ----- Movement vs Time ')
 plt.legend((' = X',' = Y',' = Z'))
 plt.show()
 # Print out Sensors
@@ -32,5 +31,21 @@ maxX = max(X_Axis)
 minY = min(Y_Axis)
 maxY = max(Y_Axis)
 
-minY = min(Y_Axis)
-maxY = max(Y_Axis)
+minZ = min(Z_Axis)
+maxZ = max(Z_Axis)
+
+print('\n-----------------------------')
+print('The min of the X Axis is %s.' % minX)
+print('The max of the X Axis is %s.' % maxX)
+print('\nThe min of the Y Axis is %s.' % minY)
+print('The max of the Y Axis is %s.' % maxY)
+print('\nThe min of the Z Axis is %s.' % minZ)
+print('The max of the Z Axis is %s.' % maxZ)
+
+seconds = ms / 1000
+minSec = float(min(seconds))
+maxSec = float(max(seconds))
+
+print('\n*****************************')
+print('The min time is %.2f' % minSec)
+print('The max time is %.2f' % maxSec)
