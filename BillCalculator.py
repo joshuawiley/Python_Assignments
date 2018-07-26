368df040a8bc1a47300c06db75eedab5b2c74ad5
joshuawiley <joshuaw1@kpit.com>
2018-07-26 10:57:58 -0400

    Read from a file instead of consol input

--- BillCalculator.py
+++ BillCalculator.py
@@ -33,6 +33,13 @@ def calculateBill(firstReading, secondReading):
 
     return bill
 
+with open('commands.txt', 'r') as f:
+    lines = f.read().splitlines()
+
+
+def get_info(lines, line_num):
+    line = lines[line_num]
+    return line.split(' | ')
 
 # Main
 list_customers = {}
@@ -44,51 +51,61 @@ while True:
     print("A/a = Add New Bill")
     print("E/e = Exit\n")
 
-    commandInput = input("Enter a command: ")
-    if commandInput in 'n' or commandInput in 'N':
-        customerID = input("Enter your 4 digit customer ID: ")
-        fRead = float(input("Enter Your Beginning of Month KwH Used: "))
-        sRead = float(input("Enter Your End of Month KwH Used: "))
+    x = len(lines)
+    for x in range(0, x):
+        COMMANDINPUT, ID, VALUE1, VALUE2 = get_info(lines, x)
 
-        # Return values
-        valid, fRead, sRead = validReading(fRead, sRead)
+        print(COMMANDINPUT)
+        print(list_customers)
 
-        if valid is True:
-            balance = float(calculateBill(fRead, sRead))
-            print("Your balance is $%.2f." % balance)
-        list_customers = {customerID: balance}
-    elif commandInput in 'l' or commandInput in 'L':
-        customerID = input("Enter your 4 digit customer ID: ")
-        if customerID not in list_customers:
-            print("\nThe Customer ID %s is not in the database." % customerID)
-        else:
-            for customerID in list_customers:
-                print(list_customers[customerID])
-    elif commandInput in 'd' or commandInput in 'D':
-        customerID = input("\nEnter your 4 digit customer ID: ")
-        del list_customers[customerID]
-        print("Customer %s has been removed from the database." % customerID)
-    elif commandInput in 'u' or commandInput in 'U':
-        # Enter current
-        customerID = input("\nEnter your current ID: ")
-        # Create a temp ID to store original ID
-        temp = customerID
-        # Get new ID
-        customerID = input("Enter your new ID: ")
-        # Set the new ID with the old ID's value
-        list_customers[customerID] = list_customers[temp]
-        # Delete the old ID
-        del list_customers[temp]
-        print("Customer ID %s has been updated to %s." % (temp, customerID))
-    elif commandInput in 'a' or commandInput in 'A':
-        customerID = input("Enter your 4 digit customer ID: ")
-        fRead = float(input("Enter Your New Beginning of Month KwH Used: "))
-        sRead = float(input("Enter Your New End of Month KwH Used: "))
+        if COMMANDINPUT in 'n' or COMMANDINPUT in 'N':
+            customerID = ID
+            fRead = float(VALUE1)
+            sRead = float(VALUE2)
 
-        # Return values
-        if valid is True:
-            balance = calculateBill(fRead, sRead)
-            print("Your balance is $%.2f." % balance)
-        list_customers = {customerID: balance}
-    elif commandInput in 'e' or commandInput in 'E':
-        exit()
+            # Return values
+            valid, fRead, sRead = validReading(fRead, sRead)
+
+            if valid is True:
+                balance = float(calculateBill(fRead, sRead))
+                print("Your user %s balance is $%.2f." % (ID, balance))
+            list_customers[customerID] = balance
+        elif COMMANDINPUT in 'l' or COMMANDINPUT in 'L':
+            customerID = ID
+            if customerID not in list_customers:
+                print("\nThe Customer ID %s is not in the database."
+                      % customerID)
+            else:
+                for customerID in list_customers:
+                    print(list_customers[customerID])
+        elif COMMANDINPUT in 'd' or COMMANDINPUT in 'D':
+            print(list_customers[ID])
+            customerID = ID
+            del list_customers[customerID]
+            print("Customer %s has been removed from the database."
+                  % customerID)
+        elif COMMANDINPUT in 'u' or COMMANDINPUT in 'U':
+            # Enter current
+            customerID = ID
+            # Create a temp ID to store original ID
+            temp = customerID
+            # Get new ID
+            customerID = VALUE1
+            # Set the new ID with the old ID's value
+            list_customers[customerID] = list_customers[temp]
+            # Delete the old ID
+            del list_customers[temp]
+            print("Customer ID %s has been updated to %s."
+                  % (temp, customerID))
+        elif COMMANDINPUT in 'a' or COMMANDINPUT in 'A':
+            fRead = float(VALUE1)
+            sRead = float(VALUE2)
+
+            # Return values
+            if valid is True:
+                balance = calculateBill(fRead, sRead)
+                print("Your user %s balance is $%.2f." % (ID, balance))
+            list_customers[ID] = balance
+        elif COMMANDINPUT in 'e' or COMMANDINPUT in 'E':
+            f.close()
+            exit()
